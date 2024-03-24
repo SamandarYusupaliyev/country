759e797db5fe43c51393209b7b88cab2f843859d
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-03-24 20:41:44 +0500

    country project

---
+++ src/js/request.js
@@ -0,0 +1,12 @@
+import loaderToggle from "./loader";
+
+
+const request =async(reource) =>{
+    loaderToggle(true)
+   const req =await fetch(reource)
+   const data =await req.json()
+   loaderToggle(false)
+   return data
+}
+
+export default request
